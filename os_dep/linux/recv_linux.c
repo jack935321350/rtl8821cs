0af809cdac1d10511d15788755f90979c4445f6b
Jack <935321350@qq.com>
2023-08-17 09:07:52 +0800

    Update.

--- os_dep/linux/recv_linux.c
+++ os_dep/linux/recv_linux.c
@@ -405,9 +405,15 @@ static int napi_recv(_adapter *padapter, int budget)
 			But dataref changed in napi_gro_receive.
 			So, we should prevent cloned SKB go into napi_gro_receive.
 		*/
-		if (pregistrypriv->en_gro && !skb_cloned(pskb)) {
+		if (pregistrypriv->en_gro) {
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0))
+			rtw_napi_gro_receive(&padapter->napi, pskb);
+			rx_ok = _TRUE;
+#else
 			if (rtw_napi_gro_receive(&padapter->napi, pskb) != GRO_DROP)
 				rx_ok = _TRUE;
+#endif
 			goto next;
 		}
 #endif /* CONFIG_RTW_GRO */
